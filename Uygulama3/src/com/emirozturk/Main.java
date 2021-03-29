1be3e8571af573e626bd1baab5862b392a850e87
Emir Öztürk <emirozturk3@gmail.com>
2021-03-29 14:39:48 +0300

    Değer türü aktarım örneği eklendi

--- Uygulama3/src/com/emirozturk/Main.java
+++ Uygulama3/src/com/emirozturk/Main.java
@@ -1,7 +1,12 @@
 package com.emirozturk;
 
 public class Main {
+    public static void fonksiyon(int deger){
+        deger = 30948;
+    }
     public static void main(String[] args) {
-
+        int degerTuru = 12837;
+        fonksiyon(degerTuru);
+        System.out.println(degerTuru);
     }
 }
