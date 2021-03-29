99126b8dbd1d59acd7a73d0dbc1e462b6f95bfbd
Emir Öztürk <emirozturk3@gmail.com>
2021-03-29 14:46:30 +0300

    Referans türü aktarım örneği eklendi

--- Uygulama3/src/com/emirozturk/Main.java
+++ Uygulama3/src/com/emirozturk/Main.java
@@ -1,12 +1,24 @@
 package com.emirozturk;
 
+class Sinif{
+    public int deger;
+}
+
 public class Main {
-    public static void fonksiyon(int deger){
+    public static void degerFonksiyonu(int deger){
         deger = 30948;
+    }
+    public static void referansFonksiyonu(Sinif nesne){
+        nesne.deger = 30948;
     }
     public static void main(String[] args) {
         int degerTuru = 12837;
-        fonksiyon(degerTuru);
+        degerFonksiyonu(degerTuru);
         System.out.println(degerTuru);
+
+        Sinif referansTuru = new Sinif();
+        referansTuru.deger = 12837;
+        referansFonksiyonu(referansTuru);
+        System.out.println(referansTuru.deger);
     }
 }
