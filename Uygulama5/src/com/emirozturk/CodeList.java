7d976807ec105c9a1803d48498cee84d79cc2607
Emir Öztürk <emirozturk3@gmail.com>
2021-04-05 15:23:26 +0300

    İkinci uygulama ilk hali enumlar ile

--- Uygulama5/src/com/emirozturk/CodeList.java
+++ Uygulama5/src/com/emirozturk/CodeList.java
@@ -1,15 +1,26 @@
 package com.emirozturk;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 enum Type{Binary,Text}
 public class CodeList {
     List<Integer> codes;
-    public CodeList(String dosyaYolu,Type tur){
+    public CodeList(String dosyaYolu,Type tur) throws IOException {
+        codes = new ArrayList<>();
         if(tur == Type.Binary){
-
+            //BU KISMIN BİLİNMESİNE GEREK YOK
+            var bytes = Files.readAllBytes(Path.of(dosyaYolu));
+            var intBuffer = ByteBuffer.wrap(bytes).asIntBuffer();
+            int[] dizi =new int[intBuffer.remaining()];
+            intBuffer.get(dizi);
+            //
+            for(var eleman:dizi)
+                codes.add(eleman);
         }
         else if(tur == Type.Text){
             var satirlar = Files.readAllLines(Path.of(dosyaYolu));
