31b2eb48a788bef631e45fd6a5cddbdd8bdfc391
Emir Öztürk <emirozturk3@gmail.com>
2021-04-12 14:59:24 +0300

    5. haftaki uygulamanın son hali

--- Uygulama5/src/com/emirozturk/CodeList.java
+++ Uygulama5/src/com/emirozturk/CodeList.java
@@ -6,33 +6,40 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CodeList {
     List<Integer> codes;
     private CodeList(List<Integer> liste){
         codes = liste;
     }
-    CodeList fromBinary(String dosyaYolu){
-        return null;
-        /*Haftaya devam edilecek
-        if(tur == Type.Binary){
+    public static CodeList fromBinary(String dosyaYolu) throws IOException{
             //BU KISMIN BİLİNMESİNE GEREK YOK
-            var bytes = Files.readAllBytes(Path.of(dosyaYolu));
-            var intBuffer = ByteBuffer.wrap(bytes).asIntBuffer();
-            int[] dizi =new int[intBuffer.remaining()];
+            var bytelar = Files.readAllBytes(Path.of(dosyaYolu));
+            var intBuffer = ByteBuffer.wrap(bytelar).asIntBuffer();
+            var elemanSayisi = intBuffer.remaining();
+            int[] dizi =new int[elemanSayisi];
             intBuffer.get(dizi);
-            //
+            List<Integer> liste = new ArrayList<>();
             for(var eleman:dizi)
-                codes.add(eleman);
-        }
-        */
+                liste.add(eleman);
+            return new CodeList(liste);
     }
     static CodeList fromText(String dosyaYolu) throws IOException {
-         var satirlar = Files.readAllLines(Path.of(dosyaYolu));
+        /*
+        var satirlar = Files.readAllLines(Path.of(dosyaYolu));
          List<Integer> liste = new ArrayList<>();
             for(var satir:satirlar)
                 liste.add(Integer.parseInt(satir));
         return new CodeList(liste);
+
+        var satirlar = Files.readAllLines(Path.of(dosyaYolu));
+        var akis = satirlar.stream();
+        var intAkis = akis.map(Integer::parseInt);
+        var liste = intAkis.collect(Collectors.toList());
+        return new CodeList(liste);
+        */
+        return new CodeList(Files.readAllLines(Path.of(dosyaYolu)).stream().map(Integer::parseInt).collect(Collectors.toList()));
     }
     public String toString() {
         StringBuilder degisken = new StringBuilder();
