8e2afbd4e39a5288397621a537d48c70ebeb9c92
Emir Öztürk <emirozturk3@gmail.com>
2021-05-24 14:50:21 +0300

    Override ve overload örnekleri eklendi

--- Uygulama8/src/com/emirozturk/Main.java
+++ Uygulama8/src/com/emirozturk/Main.java
@@ -1,21 +1,43 @@
 package com.emirozturk;
+/*
 //Aynısı statik sınıflar için de yapılabilir
 class Islem{
-    int maksimum(int a,int b,int c){
+    static int maksimum(int a,int b,int c){
         int enBuyuk = a;
         if(b>enBuyuk)enBuyuk = b;
         if(c>enBuyuk)enBuyuk = c;
         return enBuyuk;
     }
-    float maksimum(float a,float b){
+    static float maksimum(float a,float b){
         return a>b?a:b;
     }
 }
+*/
+/*
+interface Arayuz{
+    int maksimum(int a,int b);
+}
+class UstSinif implements Arayuz{
+    public int maksimum(int a, int b) {
+        System.out.println("Ust Sinif Maksimum bulma işlemini başarıyla tamamladı");
+        return a>b?a:b;
+    }
+}
+class AltSinif extends UstSinif{
+    @Override
+    public int maksimum(int a,int b){
+        System.out.println("Alt Sinif Maksimum bulma işlemini başarıyla tamamladı");
+        return a>b?a:b;
+    }
+    @Override
+    public String toString(){
+        return "x";
+    }
+}
+ */
 class Main{
     public static void main(String[] args) {
-        var islem = new Islem();
-        float enBuyuk = islem.maksimum(13.7f,26.2f);
-        var ikinci = islem.maksimum(1,5,456);
-        System.out.println(enBuyuk);
+        var degisken = new AltSinif();
+        System.out.println(degisken.maksimum(932,2345));
     }
 }
