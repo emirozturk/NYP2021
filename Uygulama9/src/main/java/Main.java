aeb09fbe9126244d7abe19fd62f5147e948a4a3d
Emir Öztürk <emirozturk3@gmail.com>
2021-05-31 14:58:48 +0300

    CerceveRengi değişkenine erişilemiyor. Çünkü private.

--- Uygulama9/src/main/java/Main.java
+++ Uygulama9/src/main/java/Main.java
@@ -8,13 +8,20 @@ abstract class Sekil implements IHesaplanabilir{
     private Color cerceveRengi;
 }
 class Dortgen extends Sekil{
-
+    public double alanHesapla(){
+        return 0.0;
+    }
+    public Color getCerceveRengi(){
+        return cerceveRengi;
+    }
 }
 class Kare extends Dortgen{
 
 }
 class Daire extends Sekil{
-
+    public double alanHesapla(){
+        return 0.0;
+    }
 }
 public class Main {
     public static void main(String[] args) {
