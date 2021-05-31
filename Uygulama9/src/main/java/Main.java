0d34f44407aeecc71d63e8dc10aeb8f9707dded8
Emir Öztürk <emirozturk3@gmail.com>
2021-05-31 15:05:58 +0300

    Abstract class'ın abstract olmayan metodu

--- Uygulama9/src/main/java/Main.java
+++ Uygulama9/src/main/java/Main.java
@@ -1,30 +1,38 @@
 import java.awt.*;
-interface IHesaplanabilir{
-    double alanHesapla();
-}
-abstract class Sekil implements IHesaplanabilir{
+
+abstract class Sekil{
     private int kenarKalinligi;
     private Color arkaplanRengi;
     private Color cerceveRengi;
-}
-class Dortgen extends Sekil{
-    public double alanHesapla(){
-        return 0.0;
-    }
     public Color getCerceveRengi(){
         return cerceveRengi;
     }
+    double alanHesapla(){
+        System.out.println("Dortgen Alanı hesabı");
+        return 0;
+    }
+}
+class Dortgen extends Sekil{
+
 }
 class Kare extends Dortgen{
 
 }
 class Daire extends Sekil{
+    @Override
     public double alanHesapla(){
+        System.out.println("Daire Alanı hesabı");
         return 0.0;
     }
 }
 public class Main {
     public static void main(String[] args) {
         //Sekil s = new Sekil(); //Abstract sınıflardan örnek alınamaz
+        var yeniDortgen = new Dortgen();
+        var yeniKare = new Kare();
+        var yeniDaire = new Daire();
+        System.out.println(yeniDortgen.alanHesapla());
+        System.out.println(yeniKare.alanHesapla());
+        System.out.println(yeniDaire.alanHesapla());
     }
 }
